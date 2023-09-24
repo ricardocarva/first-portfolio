4895d6a5f53f2f4332e3cbde44da5a5c301c67fd
ricardocarva <ricardofecarva@gmail.com>
2023-09-24 01:20:56 -0400

    Cleanedup file

--- src/link-checker.js
+++ src/link-checker.js
@@ -1,26 +1,3 @@
-// const linkChecker = require('link-checker');
-
-// function main() {
-//   const options = {
-//     filterLevel: linkChecker.HtmlUrlChecker.ALL,
-//     excludeExternalLinks: true, // Set this to true to exclude external links
-//   };
-
-//   const checker = new linkChecker.HtmlUrlChecker(options, {
-//     link: (result) => {
-//       if (result.broken) {
-//         console.error('Broken link:', result.url.resolved);
-//       }
-//     },
-//     end: () => {
-//       console.log('Link checking completed.');
-//     },
-//   });
-
-//   checker.enqueue('http://localhost'); // Change this to your website URL
-// }
-
-// main();
 
 const {SiteChecker} = require('broken-link-checker');
 
