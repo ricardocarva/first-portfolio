57a14da6d13430869b3e0621f3a030114f9e302c
ricardocarva <ricardofecarva@gmail.com>
2023-09-24 00:10:23 -0400

    Trying BLC

--- src/link-checker.js
+++ src/link-checker.js
@@ -1,23 +1,38 @@
-const linkChecker = require('link-checker');
+// const linkChecker = require('link-checker');
 
-function main() {
-  const options = {
-    filterLevel: linkChecker.HtmlUrlChecker.ALL,
-    excludeExternalLinks: true, // Set this to true to exclude external links
-  };
+// function main() {
+//   const options = {
+//     filterLevel: linkChecker.HtmlUrlChecker.ALL,
+//     excludeExternalLinks: true, // Set this to true to exclude external links
+//   };
 
-  const checker = new linkChecker.HtmlUrlChecker(options, {
-    link: (result) => {
-      if (result.broken) {
-        console.error('Broken link:', result.url.resolved);
-      }
-    },
-    end: () => {
-      console.log('Link checking completed.');
-    },
-  });
+//   const checker = new linkChecker.HtmlUrlChecker(options, {
+//     link: (result) => {
+//       if (result.broken) {
+//         console.error('Broken link:', result.url.resolved);
+//       }
+//     },
+//     end: () => {
+//       console.log('Link checking completed.');
+//     },
+//   });
 
-  checker.enqueue('http://localhost'); // Change this to your website URL
-}
+//   checker.enqueue('http://localhost'); // Change this to your website URL
+// }
 
-main();
+// main();
+
+const {SiteChecker} = require('broken-link-checker');
+
+const siteChecker = new SiteChecker(options)
+  .on('error', (error) => {})
+  .on('robots', (robots, customData) => {})
+  .on('html', (tree, robots, response, pageURL, customData) => {})
+  .on('queue', () => {})
+  .on('junk', (result, customData) => {})
+  .on('link', (result, customData) => {})
+  .on('page', (error, pageURL, customData) => {})
+  .on('site', (error, siteURL, customData) => {})
+  .on('end', () => {});
+
+siteChecker.enqueue(siteURL, customData);
